e1a0650d853a61f0b615973636cff53e78f2655a
Artur <zurto.art@gmail.com>
2022-09-17 21:10:15 +0300

    Added empty cart state.

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -15,66 +15,90 @@ export const Cart = () => {
     return (
         <div className="cart">
             <div className="cart__inner">
-                <div className="cart__header">
-                    <div className="cart__header-title">
-                        <i className="fa-solid fa-cart-shopping"></i>
-                        <h2>Корзина</h2>
-                    </div>
-                    <div className="cart__header-cleaner">
-                        <i className="fa-solid fa-trash"></i>
-                        <span
-                            className="cart__header-cleaner-text"
-                            onClick={() => dispatch(clearCart())}>
-                            Очистить корзину
+                {cartItems.length > 0 ? (
+                    <>
+                        <div className="cart__header">
+                            <div className="cart__header-title">
+                                <i className="fa-solid fa-cart-shopping"></i>
+                                <h2>Корзина</h2>
+                            </div>
+                            <div className="cart__header-cleaner">
+                                <i className="fa-solid fa-trash"></i>
+                                <span
+                                    className="cart__header-cleaner-text"
+                                    onClick={() => dispatch(clearCart())}>
+                                    Очистить корзину
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="cart__order-block">
+                            {cartItems.map((pizza) => (
+                                <CartPizza
+                                    id={pizza.id}
+                                    title={pizza.title}
+                                    imageUrl={pizza.imageUrl}
+                                    type={pizza.type}
+                                    size={pizza.size}
+                                    price={pizza.price}
+                                    key={pizza.id}
+                                    amount={pizza.amount}
+                                />
+                            ))}
+                        </div>
+
+                        <div className="cart__total">
+                            <div className="cart__total__summary">
+                                <span className="cart__total__summary-text">Всего пицц:</span>
+                                <b className="cart__total__summary-value">{cartItems.length} шт.</b>
+                            </div>
+                            <div className="cart__total__price">
+                                <span className="cart__total__price-text">Сумма заказа:</span>
+                                <b className="cart__total__price-value">
+                                    {cartItems.reduce(
+                                        (acum, cur) => acum + cur.price * cur.amount,
+                                        0,
+                                    )}{' '}
+                                    ₴
+                                </b>
+                            </div>
+                        </div>
+
+                        <div className="cart__buttons-block">
+                            <Link to="/">
+                                <div className="cart__buttons-block__button cart__buttons-block__button--back">
+                                    <i className="fa-solid fa-angle-left"></i>
+                                    <span className="cart__buttons-block__button--back-text">
+                                        Вернуться назад
+                                    </span>
+                                </div>
+                            </Link>
+                            <Link to="/pay/:id">
+                                <div className="cart__buttons-block__button cart__buttons-block__button--pay">
+                                    <span className="cart__buttons-block__button--pay-text">
+                                        Оплатить сейчас
+                                    </span>
+                                </div>
+                            </Link>
+                        </div>
+                    </>
+                ) : (
+                    <div className="cart__empty">
+                        <h2 className="cart__empty-title">Корзина пустая 😕</h2>
+                        <span className="cart__empty-text">
+                            Вероятней всего, вы не заказывали ещё пиццу. Для того, чтобы заказать
+                            пиццу, перейди на главную страницу.
                         </span>
-                    </div>
-                </div>
-
-                <div className="cart__order-block">
-                    {cartItems.map((pizza) => (
-                        <CartPizza
-                            id={pizza.id}
-                            title={pizza.title}
-                            imageUrl={pizza.imageUrl}
-                            type={pizza.type}
-                            size={pizza.size}
-                            price={pizza.price}
-                            key={pizza.id}
-                            amount={pizza.amount}
+                        <img
+                            src="/empty-cart.svg"
+                            alt="Пустая корзина"
+                            className="cart__empty-img"
                         />
-                    ))}
-                </div>
-
-                <div className="cart__total">
-                    <div className="cart__total__summary">
-                        <span className="cart__total__summary-text">Всего пицц:</span>
-                        <b className="cart__total__summary-value">{cartItems.length} шт.</b>
+                        <Link to="/">
+                            <button className="cart__empty-button">Вернуться назад</button>
+                        </Link>
                     </div>
-                    <div className="cart__total__price">
-                        <span className="cart__total__price-text">Сумма заказа:</span>
-                        <b className="cart__total__price-value">
-                            {cartItems.reduce((acum, cur) => acum + cur.price * cur.amount, 0)} ₴
-                        </b>
-                    </div>
-                </div>
-
-                <div className="cart__buttons-block">
-                    <Link to="/">
-                        <div className="cart__buttons-block__button cart__buttons-block__button--back">
-                            <i className="fa-solid fa-angle-left"></i>
-                            <span className="cart__buttons-block__button--back-text">
-                                Вернуться назад
-                            </span>
-                        </div>
-                    </Link>
-                    <Link to="/pay/:id">
-                        <div className="cart__buttons-block__button cart__buttons-block__button--pay">
-                            <span className="cart__buttons-block__button--pay-text">
-                                Оплатить сейчас
-                            </span>
-                        </div>
-                    </Link>
-                </div>
+                )}
             </div>
         </div>
     );
